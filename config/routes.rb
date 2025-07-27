1700debcb7da8990558ea30f5e512fa2c1171d34
Luke Ryan <github@lukeryan.net.nz>
2025-07-27 21:14:49 +1200

    Rubocop formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 get "/additionequations", to: "addition_equations#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
