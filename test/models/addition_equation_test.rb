8703e571e56403f8fe51863a2e58f45c825605d2
Luke Ryan <github@lukeryan.net.nz>
2025-07-27 21:12:05 +1200

    Add model tests for result

--- test/models/addition_equation_test.rb
+++ test/models/addition_equation_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class AdditionEquationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "addition equation" do
+    addition_equation = AdditionEquation.new(operand1: 1, operand2: 2, operator: "+")
+    assert_equal 3, addition_equation.result
+
+    addition_equation = AdditionEquation.new(operand1: 9, operand2: 9, operator: "+")
+    assert_equal 18, addition_equation.result
+  end
 end
