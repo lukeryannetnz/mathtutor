e1bdc2a5b966cd37787d87d16d5ad91232ec5563
Luke Ryan <github@lukeryan.net.nz>
2025-07-27 21:49:42 +1200

    Add turbo route to refresh partial with a new equation

--- app/controllers/addition_equations_controller.rb
+++ app/controllers/addition_equations_controller.rb
@@ -2,4 +2,26 @@ class AdditionEquationsController < ApplicationController
   def index
     @addition_equation = AdditionEquation.random
   end
+
+  def random
+    @addition_equation = AdditionEquation.random
+    respond_to do |format|
+      format.turbo_stream { 
+        render turbo_stream: turbo_stream.replace(
+          "equation-container",
+          partial: "equation",
+          locals: { addition_equation: @addition_equation }
+        )
+      }
+      format.html { render partial: 'equation', locals: { addition_equation: @addition_equation } }
+      format.json { 
+        render json: {
+          operand1: @addition_equation.operand1,
+          operand2: @addition_equation.operand2,
+          operator: @addition_equation.operator,
+          result: @addition_equation.result
+        }
+      }
+    end
+  end
 end
