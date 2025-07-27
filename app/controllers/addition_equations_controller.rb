863d07b2fe7c42bef71195057c2eccce2aeba104
Luke Ryan <github@lukeryan.net.nz>
2025-07-27 22:07:05 +1200

    Format

--- app/controllers/addition_equations_controller.rb
+++ app/controllers/addition_equations_controller.rb
@@ -6,14 +6,14 @@ class AdditionEquationsController < ApplicationController
   def random
     @addition_equation = AdditionEquation.random
     respond_to do |format|
-      format.turbo_stream { 
+      format.turbo_stream {
         render turbo_stream: turbo_stream.replace(
           "equation-container",
           partial: "equation",
           locals: { addition_equation: @addition_equation }
         )
       }
-      format.html { render partial: 'equation', locals: { addition_equation: @addition_equation } }
+      format.html { render partial: "equation", locals: { addition_equation: @addition_equation } }
     end
   end
 end
