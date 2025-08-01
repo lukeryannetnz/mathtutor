160928fa235b346784d865299f7623c331cc39f8
Luke Ryan <github@lukeryan.net.nz>
2025-08-01 20:39:31 +1200

    Add app.rb for Lambda handler and update lambda_handler.rb to define lambda_handler method; modify Dockerfile to reflect new entrypoint

--- lambda_handler.rb
+++ lambda_handler.rb
@@ -1,4 +1,11 @@
-require 'lamby'
 require_relative 'config/environment'
 
-Lamby.cmd(Lamby::Command.new)
+def lambda_handler(event:, context:)
+  {
+    statusCode: 200,
+    body: "Hello from MathTutor Rails App!",
+    headers: {
+      'Content-Type' => 'application/json'
+    }
+  }
+end
